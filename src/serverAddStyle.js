c976336d7a86f6d2587ef68951372dc8c16054fa
凌 <myywlc@gmail.com>
2019-06-17 11:45:44 +0800

    fix

--- src/serverAddStyle.js
+++ src/serverAddStyle.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default (DecoratedComponent, styles) => class NewComponent extends Component {
   componentWillMount() {
-    const { staticContext } = this.props
+    const { staticContext } = this.props;
     if (staticContext) {
       staticContext.css.push(styles._getCss());
     }
