145519aec1dd0a011cf10d7a42f14cf51993b12d
凌 <myywlc@gmail.com>
2019-06-17 15:33:27 +0800

    fix

--- src/server/index.js
+++ src/server/index.js
@@ -9,40 +9,48 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.use('/api', proxy('http://localhost:8080', {
-  proxyReqPathResolver(req) {
-    return `/ssr/api${req.url}`;
-  },
-}));
+app.use(
+  '/api',
+  proxy('http://localhost:8080', {
+    proxyReqPathResolver(req) {
+      return `/ssr/api${req.url}`;
+    }
+  })
+);
 
 app.get('*', (req, res) => {
   const store = getServerStore(req);
   const matchedRoutes = matchRoutes(routes, req.path);
   const promises = [];
 
-  matchedRoutes.forEach((item) => {
+  matchedRoutes.forEach(item => {
     if (item.route.loadData) {
       const promise = new Promise((resolve, reject) => {
-        item.route.loadData(store).then(resolve).catch(resolve);
+        item.route
+          .loadData(store)
+          .then(resolve)
+          .catch(resolve);
       });
       promises.push(promise);
     }
   });
 
-  Promise.all(promises).then(() => {
-    const context = { css: [] };
-    const html = render(store, routes, req, context);
-    if (context.action === 'REPLACE') {
-      res.redirect(301, context.url);
-    } else if (context.notFind) {
-      res.status(404);
-      res.send(html);
-    } else {
-      res.send(html);
-    }
-  }).catch(() => {
-    res.send('sorry, request error');
-  });
+  Promise.all(promises)
+    .then(() => {
+      const context = { css: [] };
+      const html = render(store, routes, req, context);
+      if (context.action === 'REPLACE') {
+        res.redirect(301, context.url);
+      } else if (context.notFind) {
+        res.status(404);
+        res.send(html);
+      } else {
+        res.send(html);
+      }
+    })
+    .catch(() => {
+      res.send('sorry, request error');
+    });
 });
 
 app.listen(3000);
